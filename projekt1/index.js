5c701786c4caf314476965ed09c774c4c699d6a7
Kacper Krusinski <kacper.krusinski@microsoft.wsei.edu.pl>
2022-03-16 18:10:27 +0100

    kalk1

--- projekt1/index.js
+++ projekt1/index.js
@@ -1,6 +1,35 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World');
+// app.get('/', function (req, res) {
+//     res.send('Hello World');
+// }
+
+app.get('/dodaj/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+    result= a+b;
+    res.send(`Wynik dodawania ${result}`);
 });
+
+app.get('/usun/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+    result= a-b;
+    res.send(`Wynik odejmowania ${result}`);
+});
+
+app.get('/podziel/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+    result= a/b;
+    res.send(`Wynik dzielenia ${result}`);
+});
+
+app.get('/pomnoz/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+    result= a*b;
+    res.send(`Wynik mnozenie ${result}`);
+});
+
 app.listen(3000);
